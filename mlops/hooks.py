a85512250c36bf0e935bcb94b46f519a31b84be3
isabellabarcelossg <isabella.goncalves@ipiranga.ipiranga>
2023-07-05 18:10:24 -0300

    create hooks.py

---
+++ mlops/hooks.py
@@ -0,0 +1,37 @@
+import pymsteams
+
+class Hooks:
+
+    @staticmethod
+    def generate_payload(title: str, message: str, severity: str) -> dict:
+        severity_colors = {
+            "high": "FF0000",   # Red color
+            "medium": "FFFF00",  # Yellow color
+            "low": "00FF00"      # Green color
+        }
+
+        color = severity_colors.get(severity, "FFFFFF")  # Default to white
+
+        payload = {
+            "text": message,
+            "themeColor": color,
+            "title": title,
+        }
+
+        return payload
+
+    @staticmethod
+    def send_teams_alert(alert_dict: dict) -> None:
+        # Establishes the connection with the Teams webhook
+        teams = pymsteams.connectorcard("link")
+        # Extract alert information from the dictionary
+        title = alert_dict["title"]
+        message = alert_dict["message"]
+        severity = alert_dict["severity"]
+
+        # Generate payload
+        payload = Hooks.generate_payload(title, message, severity)
+        teams.payload = payload
+
+        # Send alert
+        teams.send()
