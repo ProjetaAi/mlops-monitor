12b8c1f40b6e6fdcfc996b59ad6b1ce3824fef65
isabellabarcelossg <isabella.goncalves@ipiranga.ipiranga>
2023-07-07 14:08:49 -0300

    add check dict and set webhook as an environment variable

--- mlops/hooks.py
+++ mlops/hooks.py
@@ -1,4 +1,8 @@
 import pymsteams
+from dotenv import load_dotenv
+import os
+import logging
+
 
 class Hooks:
 
@@ -19,11 +23,36 @@ class Hooks:
         }
 
         return payload
+    
+    @staticmethod
+    def get_webhook() -> str:
+        #Load environment and return webhook str.
+
+        load_dotenv()
+        return os.getenv('WEBHOOK_URL')
+    
+    @staticmethod
+    def check_dict(alert_dict: dict) -> str:
+        # Check input.
+        required_fields = ["title", "message", "severity"]
+        missing_fields = [field for field in required_fields if field not in alert_dict]
+
+        if missing_fields:
+            error_message = f"Os seguintes campos estão faltando: {', '.join(missing_fields)}"
+            raise ValueError(error_message)
 
     @staticmethod
-    def send_teams_alert(alert_dict: dict) -> None:
+    def send_teams_alert(alert_dict: dict) -> None: 
+        #Check dictionary fields.
+        try:
+            Hooks.check_dict(alert_dict)
+        except ValueError as e:
+            logging.error(e)
+            raise ValueError(e)
+        
         # Establishes the connection with the Teams webhook
-        teams = pymsteams.connectorcard("link")
+        teams = pymsteams.connectorcard(Hooks.get_webhook())
+
         # Extract alert information from the dictionary
         title = alert_dict["title"]
         message = alert_dict["message"]
@@ -35,3 +64,4 @@ class Hooks:
 
         # Send alert
         teams.send()
+        
